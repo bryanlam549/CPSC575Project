1c69314172570e28ea0a6f4ad2618a067ff19ce2
bryan lam <bryanlam549@hotmail.com>
2020-10-18 15:35:52 -0600

    Added TestView

---
+++ CPSC575Project/Views/TestView.swift
@@ -0,0 +1,43 @@
+//
+//  TestView.swift
+//  CPSC575Project
+//
+//  Created by bryan lam on 2020-10-18.
+//  Copyright © 2020 bryan lam. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    //hardcoded data
+    //let data = testData
+    
+    //Make use of the view model
+    @ObservedObject private var viewModel = VMtest()
+    
+    //Hardcoded test values
+    var newCollection = TestCollection(col1: "test", col2: "test", column3: 9)
+    var body: some View {
+        VStack {
+            Text("TEST LIST")
+            List(viewModel.testCollections){ testCol in
+                Text(testCol.col1)
+            }
+            .onAppear(){
+                self.viewModel.fetchData()
+            }
+            
+            Button(action: {self.viewModel.addData(collection: self.newCollection)}) {
+                
+                Text("Add hardcoded values...")
+                
+            }
+            
+            
+        }
+        
+    }
+}
+
+
+
