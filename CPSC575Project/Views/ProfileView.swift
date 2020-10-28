9888ac5306a22f8cc7766b059641c36fc3d21d37
Selma Novl <selma.cpsc@gmail.com>
2020-10-28 14:15:27 -0600

    implementation of profile UI

--- CPSC575Project/Views/ProfileView.swift
+++ CPSC575Project/Views/ProfileView.swift
@@ -1,34 +1,66 @@
-//
-//  ProfileView.swift
-//  CPSC575Project
-//
-//  Created by bryan lam on 2020-10-18.
-//  Copyright © 2020 bryan lam. All rights reserved.
-//
+        //
+        //  ProfileView.swift
+        //  CPSC575Project
+        //
+        //  Created by bryan lam on 2020-10-18.
+        //  Copyright © 2020 bryan lam. All rights reserved.
+        //
 
-import SwiftUI
-import Firebase
+        import SwiftUI
+        import Firebase
 
-struct ProfileView: View {
-    @EnvironmentObject var model: LoginSignUpModel // for testing
-    
-    var body: some View {
-        VStack{
-            Text("Profile View")
-            //Log out button TESTING
-            Button(action:{
-                //THIS IS A TEST...But this is how you'd sign out
-                self.model.logOut()
-            }){
-                Text("Sign out")
-            }
-        }
-        
-    }
-}
+        struct ProfileView: View {
+            @EnvironmentObject var model: LoginSignUpModel // for testing
+            
+            var body: some View {
+                GeometryReader { geo in
+                            
+                            VStack {
+                                TopView().padding(.bottom)
+                                Text("Profile")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                
+                                ZStack {
+                                    Image("profile")
+                                        .resizable()
+                                        .frame(width: geo.size.width / 1.5, height: geo.size.width / 1.5, alignment: .center)
+                                        .cornerRadius((geo.size.width / 1.5) / 2)
+                                    
+                                    VStack {
+                                        Spacer()
+                                        HStack {
+                                            Spacer()
+                                            Button(action: profileEditButtonTapped) {
+                                                Text("Edit")
+                                            }
+                                        }
+                                    }
+                                    
+                                }.frame(width: geo.size.width / 1.5, height: geo.size.width / 1.5, alignment: .center)
+                                
+                                Text("Jane Doe, 22")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                
+                                Text("Business Studnet @ UofC\n\nI love my dogs and staying active:)\n\nContact me ").padding().lineLimit(6)
+                                Spacer()
+                            }
+                        }
+                    }
+                    
+                    func profileEditButtonTapped() {
+                        print("Profile Edit Button tapped...")
+                    }
+                }
 
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
-    }
-}
+                struct ProfileView_Previews: PreviewProvider {
+                    static var previews: some View {
+                        Group {
+                            ProfileView()
+                            ProfileView()
+                                .previewDevice("iPhone 11")
+                        }
+                    }
+                }
+
