c2ba70ddb79997e8e46b824c0645a4b506dd690d
snovl <56573358+snovl@users.noreply.github.com>
2020-11-26 13:40:01 -0700

    Create EditObj.swift
    update controller

---
+++ CPSC575Project/ProfileScreen/EditObj.swift
@@ -0,0 +1,21 @@
+//
+//  EditobjViewController.swift
+//  CPSC575Project
+//
+//  Created by Selma Novl on 26/11/20.
+//  Copyright © 2020 bryan lam. All rights reserved.
+//
+import UIKit
+
+class EditobjViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
